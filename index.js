aedc23d3e793578ce61170a7c138826f9f761996
shamim <shamimmahmud077@gmail.com>
2024-02-07 23:24:54 +0600

    almost solved problem two

--- index.js
+++ index.js
@@ -15,5 +15,41 @@ function calculateMoney(ticketSale) {
   return result;
 }
 
-const output = calculateMoney(-130);
+// const output = calculateMoney(-130);
+// console.log(output);
+
+function checkName(name) {
+  let arr = ["A", "y", "i", "e", "o", "u", "w"];
+  let caseCompatibility = arr.toString().toLowerCase();
+
+  let lastChar = name[name.length - 1];
+
+  if (typeof name !== "string") {
+    return "Invalid";
+  }
+
+  if (caseCompatibility.includes(lastChar)) {
+    return "Good Name";
+  } else {
+    return "Bad Name";
+  }
+}
+
+const output = checkName("mahbuba");
 console.log(output);
+
+// function checkName(name){
+//     last_chars = ['A', 'Y', 'I', 'E', 'O', 'U', 'W']
+//     last_char = last_chars[last_chars.length - 1]
+//     // arr[arr.length - 1]
+
+// if (last_char in last_chars)
+//     return "Good Name"
+// else{
+//     return "Bad Name"
+//     }
+
+// }
+
+//    const output = checkName('shamiE')
+//    console.log(output);

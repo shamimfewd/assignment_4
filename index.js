9aeebb779a8afb210409fad7dd871e65c6e0bca9
shamim <shamimmahmud077@gmail.com>
2024-02-07 22:01:10 +0600

    solved the frist problem

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function calculateMoney(ticketSale) {
   const perTicket = 120;
-  let gatekeeperCost = 500;
-  let staffs = 8;
+  const gatekeeperCost = 500;
+  const staffs = 8;
+
+  if (ticketSale < 0) {
+    return "Invalid Number";
+  }
 
   const ticketQuantity = ticketSale * perTicket;
   const staffsCost = staffs * 50;
-
   const totalCost = gatekeeperCost + staffsCost;
+
   const result = ticketQuantity - totalCost;
   return result;
 }
 
 const output = calculateMoney(-130);
-
 console.log(output);

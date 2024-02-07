06d964ba4f2c0a0c3a4e9e281a473d4150c23cc5
shamim <shamimmahmud077@gmail.com>
2024-02-07 21:28:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+function calculateMoney(ticketSale) {
+  const perTicket = 120;
+
+  const ticketQuantity = ticketSale * perTicket
+
+  return ticketQuantity
+}
+
+const output = calculateMoney(10)
+
+console.log(output);

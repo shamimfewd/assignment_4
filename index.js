607e678f6ecc6c1b0c2db959f1765f2110b70b5d
shamim <shamimmahmud077@gmail.com>
2024-02-07 21:48:38 +0600

    finished the problem one

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-
-
-
-
-
-
-
-
 function calculateMoney(ticketSale) {
   const perTicket = 120;
+  let gatekeeperCost = 500;
+  let staffs = 8;
 
-  const ticketQuantity = ticketSale * perTicket
+  const ticketQuantity = ticketSale * perTicket;
+  const staffsCost = staffs * 50;
 
-  return ticketQuantity
+  const totalCost = gatekeeperCost + staffsCost;
+  const result = ticketQuantity - totalCost;
+  return result;
 }
 
-const output = calculateMoney(10)
+const output = calculateMoney(-130);
 
 console.log(output);
